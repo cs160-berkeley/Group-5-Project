56c51c597167817a4b4f06f8c57666e88778dbb6
Graham Seyffert <gseyffert@berkeley.edu>
2016-04-19 17:15:19 -0700

    Pensieve Skeleton

---
+++ PensieveSkeleton/wear/src/main/java/com/example/gseyf/pensieveskeleton/AlarmNotificationActivity.java
@@ -0,0 +1,17 @@
+package com.example.gseyf.pensieveskeleton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AlarmNotificationActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        mTextView = (TextView) findViewById(R.id.text);
+    }
+}

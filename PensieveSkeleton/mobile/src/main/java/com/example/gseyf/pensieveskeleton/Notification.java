90bf7a6805f6524b0611e8bc38574c5d386c31f1
David <mydavidkoh@gmail.com>
2016-04-20 15:44:54 -0700

    notification is good

---
+++ PensieveSkeleton/mobile/src/main/java/com/example/gseyf/pensieveskeleton/Notification.java
@@ -0,0 +1,23 @@
+package com.example.gseyf.pensieveskeleton;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by david on 4/20/16.
+ */
+public class Notification extends Activity {
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.family_member_notification_activity);
+    }
+
+    public void hesOk(View view){
+        Intent i = new Intent(this, FamilyMemberMainActivity.class);
+        startActivity(i);
+    }
+}
---
+++ PensieveSkeleton/mobile/src/main/java/com/example/gseyf/pensieveskeleton/NotificationResolved.java
@@ -0,0 +1,18 @@
+package com.example.gseyf.pensieveskeleton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by david on 4/20/16.
+ */
+public class NotificationResolved extends Activity {
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.notification_resolved);
+    }
+
+
+}
